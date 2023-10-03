4e1088ebe06ddedee2013f25f4a359f190e38be6
Aaron Sullivan <aasullivan1618@gmail.com>
2023-10-03 15:33:39 -0700

    Add link to Posts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,6 +9,15 @@ export default function Index() {
   const user = useOptionalUser();
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+      <div className="mx-auto mt-16 max-w-7xl text-center">
+        <Link
+          to="/posts"
+          className="text-xl text-blue-600 underline"
+        >
+          Blog Posts
+        </Link>
+      </div>
+
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
