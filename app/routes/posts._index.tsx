f60769df845fb7c9387655c199dee838ac7e56e7
Aaron Sullivan <aasullivan1618@gmail.com>
2023-10-06 13:42:48 -0700

    WIP Deletion from /posts

--- app/routes/posts._index.tsx
+++ app/routes/posts._index.tsx
@@ -1,9 +1,26 @@
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
-import { getPosts } from "~/models/post.server";
+import { ActionFunctionArgs, json } from "@remix-run/node";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { deletePost, getPosts } from "~/models/post.server";
 
 export const loader = async () => {
   return json({ posts: await getPosts() });
+};
+
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  const formData = await request.formData();
+  const slug = formData.get("slug");
+
+  invariant(slug, "Must supply slug");
+
+  if (typeof slug !== "string" || slug.length === 0) {
+    return json(
+      { errors: { message: "Must supply slug" } },
+      { status: 400 },
+    );
+  }
+
+  await deletePost(slug);
 };
 
 export default function Posts() {
@@ -14,13 +31,15 @@ export default function Posts() {
       <Link to="new">Make a new one</Link>
       <ul>
         {posts.map((post) => (
-          <li key={post.slug}>
+          <li key={post.slug} className="flex">
             <Link
               to={post.slug}
               className="text-blue-600 underline"
             >
               {post.title}
             </Link>
+            <span>|</span>
+            <Form action={`/posts/${post.slug}`} method="delete"><button type="submit">Deltaco The Thing</button></Form>
           </li>
         ))}
       </ul>
