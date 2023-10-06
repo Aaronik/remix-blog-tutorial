dfde1ba1dccd02767e7ed4990fabdb4ae44878bc
Joshua Aresty <jaresty@gmail.com>
2023-10-06 09:48:31 -0700

    use database to serve posts

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,13 @@
+import { prisma } from "~/db.server";
+
 type Post = {
   slug: string;
   title: string;
 };
 
 export async function getPosts(): Promise<Array<Post>> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+  return prisma.post.findMany({
+    select: { slug: true, title: true },
+    orderBy: { updatedAt: "desc" },
+  });
 }
